24ad43f75f8a41f82d9187fedfc627b700f7a3f1
Martin Kruger <martin@martinkruger.com>
2020-02-03 19:17:47 +0200

    Updates

---
+++ precache-manifest.e8bb26c65cbc96b8ac0aab8ea540f897.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9146b1324e00d5fcbd06e638f8bf5718",
+    "url": "/Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "0dd09565e7f837ebdd57",
+    "url": "/Random-Quote-Machine/static/css/main.328a1124.chunk.css"
+  },
+  {
+    "revision": "1e47ee80b67c7f34d8f7",
+    "url": "/Random-Quote-Machine/static/js/2.e97a2037.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/Random-Quote-Machine/static/js/2.e97a2037.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0dd09565e7f837ebdd57",
+    "url": "/Random-Quote-Machine/static/js/main.5b2bec62.chunk.js"
+  },
+  {
+    "revision": "6d9732fc5364ec9a464f",
+    "url": "/Random-Quote-Machine/static/js/runtime-main.769b331a.js"
+  },
+  {
+    "revision": "5922e6c697425a01c7c0368f7f4de7fd",
+    "url": "/Random-Quote-Machine/static/media/bg3.5922e6c6.jpg"
+  }
+]);
